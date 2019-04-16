7801f641de58864b9af5b0b657da867d7d39b2df
Chris Handy <maximinus@gmail.com>
2019-04-16 18:54:31 +0100

    Converter cycles through WAVS to slice

--- src/utility/helpers.py
+++ src/utility/helpers.py
@@ -14,7 +14,7 @@ def getRootDirectory():
 		filepath = os.path.dirname(os.path.realpath(__file__))
 		filepath = os.path.join(filepath, '../..')
 		# the root folder is 2 folders above this
-		return filepath
+		return os.path.normpath(filepath)
 	except:
 		return ''
 
@@ -45,44 +45,39 @@ def clearDirectory(path):
 	os.mkdir(path)
 
 
-class Slicer:
-	# slicer has the job of taking an input directory and slicing all the files
-	# to an output directory
-	def __init__(self, files, directory):
-		self.index = 0
-		self.dir_index = 0
-		self.files = files
-		self.output = '{0}/data/SLICES/'
-		clearDirectory(self.output)
+def sliceAsWav(wav_file, output_folder):
+	file_index = 0
+	print('  > Slicing {0} as WAV'.format(wav_file))
+	sound = AudioSegment.from_file(wav_file, format='wav')
+	duration = len(sound)
+	total_slices = len(sound) // TIMESLICE
+	for i in range(total_slices):
+		new_slice = sound[i * TIMESLICE: (i + 1) * TIMESLICE]
+		print('{0}/{1}.wav'.format(output_folder, getStringName(file_index)))
+		#new_slice.export('{1}/{2}.wav'.format(output_folder, self.getFilename('wav')), format='wav')
+		file_index += 1
 
-	def getFilename(self, extension='raw'):
-		number = str(self.index)
-		self.index += 1
-		return '{0}{1}.{2}'.format('0' * (6 - len(number)), number, extension)
 
-	def getDirectoryName(self):
-		number = str(self.dir_index)
-		self.dir_index += 1
-		return '{0}{1}'.format('0' * (4 - len(number)), number)
-
-	def sliceAsWav(self):
-		total = 0
-		for filename in self.files:
-			print('  > Slicing {0} as WAV'.format(filename))
-			dir_name = self.getDirectoryName()
-			os.mkdir('{0}/{1}'.format(self.output, dir_name))
-			sound = AudioSegment.from_file(filename, format='wav')
-			duration = len(sound)
-			total_slices = len(sound) // TIMESLICE
-			#for i in range(total_slices):
-			#	new_slice = sound[i * TIMESLICE: (i + 1) * TIMESLICE]
-			#	new_slice.export('{0}/{1}/{2}'.format(self.output, dir_name, self.getFilename('wav')), format='wav')
-			#	total += 1
-		return total
+def getStringName(index):
+	number = str(index)
+	return '{0}{1}'.format('0' * (4 - len(number)), number)
 
 
 def sliceFiles(root_folder, destination_folder):
 	print('* Slicing files in {0} to {1}'.format(root_folder, destination_folder))
+	clearDirectory(destination_folder)
 	# we need to recreate all the files in the root
 	for i in [f.path for f in os.scandir(root_folder) if f.is_dir()]:
-		print('--> {0}'.format(i))
+		folder_count = 0
+		for j in getAllFiles(i):
+			# get the subdir folder name
+			root = i.split('/')[-1]
+			# add it to the destination folder
+			dest_folder = os.path.join(destination_folder, root)
+			# if it does not exist, create it
+			try:
+				os.makedirs(dest_folder)
+			except:
+				pass
+			sliceAsWav(j, dest_folder)
+			folder_count += 1
