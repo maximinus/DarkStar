86358d843cd506bd748628fa4c60d3570233fa18
maximinus <maximinus@gmail.com>
2020-05-22 15:02:33 +0100

    First experiment loads data files

--- mount/experiments/exp1/discriminator.py
+++ mount/experiments/exp1/discriminator.py
@@ -11,6 +11,9 @@ import warnings
 warnings.filterwarnings("ignore")
 
 import os
+import sys
+from tqdm import tqdm
+import random
 import tensorflow as tf
 from tensorflow.keras.layers import Conv1D, Dense, Dropout
 import numpy as np
@@ -22,19 +25,48 @@ TOTAL_CLASSES = 1
 AUD_SLICES = 'music/slices/aud'
 SBD_SLICES = 'music/slices/sbd'
 
+
+class ModelData:
+    CHECK_RATIO = 0.2
+
+    def __init__(self, aud, sbd):
+        # randomise the 2 arrays and split up
+        random.shuffle(aud)
+        random.shuffle(sbd)
+        check = int(len(aud) * self.CHECK_RATIO)
+        self.test_aud = aud[check:]
+        self.check_aud = aud[:check]
+        check = int(len(sbd) * self.CHECK_RATIO)
+        self.test_sbd = sbd[check:]
+        self.check_sbd = sbd[:check]
+
+    def __repr__(self):
+        atext = f'[{len(self.check_aud)}, {len(self.test_aud)}]'
+        stext = f'[{len(self.check_sbd)}, {len(self.test_sbd)}]'
+        return f'ModelData: Aud {atext}, Sbd {stext}'
+
+
 def loadAllFiles(folder):
     # ensure fodler exists
-    if not is.path.isdir(folder):
+    if not os.path.isdir(folder):
         helpers.logError(f'{folder} is not a valid folder')
-    # recurse to get all raw files here
-    return helpers.getAllFiles(folder, extension='raw')
+    files = helpers.getAllFiles(folder, extension='raw')
+    audio = []
+    for i in tqdm(files):
+        audio_data = np.fromfile(i, dtype=np.uint16, count=-1, sep='', offset=0)
+        # convert to a float
+        float_data = audio_data.astype(np.float32)
+        # then convert to the range 0 -> 1
+        float_data /= 65536
+        audio.append(float_data)
+    return audio
 
 def getData():
     # load everything as a dataset, since it will fit into memory
     root_folder = helpers.getRootDirectory()
     aud_files = loadAllFiles(os.path.join(root_folder, AUD_SLICES))
     sbd_files = loadAllFiles(os.path.join(root_folder, SBD_SLICES))
-    return [aud_files, sbd_files]
+    return ModelData(aud_files, sbd_files)
 
 def getDiscriminator():
     model = tf.keras.models.Sequential()
@@ -51,6 +83,7 @@ def getDiscriminator():
 
 if __name__ == '__main__':
     data = getData()
+    print(data)
     sys.exit()
 
     print(f'Data shape: {data.shape}')
